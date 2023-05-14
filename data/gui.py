a8dcd0cf34bb27b235f7882291f7533322b8147b
CatNJ <94224244+CatNJ@users.noreply.github.com>
2023-05-14 21:12:18 +0300

    Add coments & fix

--- data/gui.py
+++ data/gui.py
@@ -1,10 +1,11 @@
 import pygame
 
+# імпорт вікна гри
 from data.game_window import root
 
 pygame.init()
 
-
+# клас
 class Area():
     def __init__(self, x=0, y=0, width=10, height=10, color=(0,0,0), window=root):
         self.window = window
@@ -15,17 +16,22 @@ class Area():
         self.color = color
         self.color_bak = color
 
+        # створення прямокутника
         self.rect = pygame.Rect(x, y, width, height)
 
+    # зміна кольору на новий
     def color(self, new_color):
         self.color = new_color
 
+    # малювання прямокутника на екрані
     def fill(self):
         pygame.draw.rect(self.window, self.color, self.rect)
 
+    # точка пересічення
     def collidepoint(self, pos):
         return self.rect.collidepoint(pos)
 
+    # пересічення з іншим прямокутником
     def colliderect(self, rect):
         return self.rect.colliderect(rect)
 
@@ -42,8 +48,9 @@ class Label(Area):
         else:
             self.color = self.color_bak
 
+    # малюєм прямокунтник з текстом
     def draw(self, shift_x=0, shift_y=0):
-        pygame.draw.rect(self.window, self.color, self.rect)
+        self.fill()
 
         if self.text != '':
             self.window.blit(self.text, (self.rect.x + shift_x, self.rect.y + shift_y))
@@ -51,5 +58,6 @@ class Label(Area):
 
 class Button(Label):
     def is_clicked(self):
+        # якщо мишка наведина на кнопку і нажата ліваклавіша миші
         if self.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
             return True
